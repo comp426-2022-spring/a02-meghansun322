6d5aa729c60685eab4dd433a54db4b3b705a7f3e
Meghan Sun <meghansun322@gmail.com>
2022-02-03 16:28:40 -0500

    made a fake coin flip

--- modules/coin.mjs
+++ modules/coin.mjs
@@ -1,21 +1,29 @@
-/** Coin flip functions 
+/** Coin flip functions
  * This module will emulate a coin flip given various conditions as parameters as defined below
  */
 
 /** Simple coin flip
- * 
+ *
  * Write a function that accepts no parameters but returns either heads or tails at random.
- * 
+ *
  * @param {*}
- * @returns {string} 
- * 
+ * @returns {string}
+ *
  * example: coinFlip()
  * returns: heads
- * 
+ *
  */
 
-function coinFlip() {
+export function coinFlip() {
+  var result;
+  var rand_num = Math.random();
 
+  if (rand_num < 0.5) {
+    result = "heads";
+  } else {
+    result = "tails";
+  }
+  return result;
 }
 
 /** Multiple coin flips
@@ -37,44 +45,37 @@ function coinFlip() {
     ]
  */
 
-function coinFlips(flips) {
-
-}
+function coinFlips(flips) {}
 
 /** Count multiple flips
- * 
- * Write a function that accepts an array consisting of "heads" or "tails" 
- * (e.g. the results of your `coinFlips()` function) and counts each, returning 
+ *
+ * Write a function that accepts an array consisting of "heads" or "tails"
+ * (e.g. the results of your `coinFlips()` function) and counts each, returning
  * an object containing the number of each.
- * 
+ *
  * example: conutFlips(['heads', 'heads','heads', 'tails','heads', 'tails','tails', 'heads','tails', 'heads'])
  * { tails: 5, heads: 5 }
- * 
- * @param {string[]} array 
+ *
+ * @param {string[]} array
  * @returns {{ heads: number, tails: number }}
  */
 
-function countFlips(array) {
-
-}
+function countFlips(array) {}
 
 /** Flip a coin!
- * 
- * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose". 
- * 
- * @param {string} call 
+ *
+ * Write a function that accepts one input parameter: a string either "heads" or "tails", flips a coin, and then records "win" or "lose".
+ *
+ * @param {string} call
  * @returns {object} with keys that are the input param (heads or tails), a flip (heads or tails), and the result (win or lose). See below example.
- * 
+ *
  * example: flipACoin('tails')
  * returns: { call: 'tails', flip: 'heads', result: 'lose' }
  */
 
-function flipACoin(call) {
+function flipACoin(call) {}
 
-}
-
-
-/** Export 
- * 
+/** Export
+ *
  * Export all of your named functions
-*/
+ */
