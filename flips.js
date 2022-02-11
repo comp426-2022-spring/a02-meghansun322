e6ad83644ed6e6d5e80e08c37b7d46fbd843805a
Meghan Sun <meghansun322@gmail.com>
2022-02-11 10:30:49 -0500

    changed back

--- flips.js
+++ flips.js
@@ -2,7 +2,7 @@ import { coinFlips, countFlips } from "./modules/coin.js";
 import minimist from "minimist";
 
 const args = minimist(process.argv.slice(2));
-const flip_count = args["number"] || 5;
+const flip_count = args["number"] || 1;
 const flip_list = coinFlips(flip_count);
 console.log(flip_list);
 console.log(countFlips(flip_list));
